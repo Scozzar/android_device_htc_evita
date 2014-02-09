54fade8296d83d3140ad1e762d65d6dd4387cd5f
Scozzar <scazzizzle@gmail.com>
2014-02-08 23:29:38 -0800

    Update omni-evita
    Basically, I replaced the word "jewel" with "evita" so if for whatever reason I need to revert...there is is.

--- omni_jewel.mk
+++ omni_jewel.mk
@@ -4,20 +4,20 @@ $(call inherit-product, vendor/omni/config/cdma.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/jewel/jewel.mk)
+$(call inherit-product, device/htc/evita/evita.mk)
 
 # Device naming
-PRODUCT_DEVICE := jewel
-PRODUCT_NAME := omni_jewel
+PRODUCT_DEVICE := evita
+PRODUCT_NAME := omni_evita
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := EVO
+PRODUCT_MODEL := One X
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jewel BUILD_ID=JRO03C BUILD_FINGERPRINT="htc/jewel/jewel:4.1.1/JRO03C/147243.3:user/release-keys" PRIVATE_BUILD_DESC="3.16.651.3 CL147243 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=evita BUILD_ID=JRO03C BUILD_FINGERPRINT="htc/jewel/jewel:4.1.1/JRO03C/147243.3:user/release-keys" PRIVATE_BUILD_DESC="3.16.651.3 CL147243 release-keys"
 
 # Release name
-PRODUCT_RELEASE_NAME := jewel
+PRODUCT_RELEASE_NAME := evita
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
